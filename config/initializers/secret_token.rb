b87a1eb65b03b6a94eb7fc5aa97ffb7eb0b5372e
Amrit Ayalur <a.ayalur@gmail.com>
2013-12-24 17:45:57 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sepwebsite::Application.config.secret_token = 'add185c853aab4e249ef123b4c797af86d72f344e47edbebfa65f231e953f342f7572d078a2ca640d50f123cd2a2c77d7cec9a44832382d807c1eb2fa96bde1d'
