6a071c7d219d87980c76b52b95392100a76755e1
Amrit Ayalur <a.ayalur@gmail.com>
2014-01-20 15:37:33 -0800

    tryna fix image error

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
 	def index
-		@brothers = Brother.all
+		@brothers = Brother.where(['status <> ?', 'Alumnus']).sort_by!{ |e| e.name.downcase }
 		@application = Application.new
 		@alumni = Brother.where(:status => "Alumnus").sort_by!{ |m| m.name.downcase }
 	end
