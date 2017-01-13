7bf5a78a2e7bfe2603fd215186c94e8593e9e208
Amrit Ayalur <a.ayalur@gmail.com>
2017-01-13 10:48:01 -0600

    Prettify code.

--- app/controllers/rush_applications_controller.rb
+++ app/controllers/rush_applications_controller.rb
@@ -2,30 +2,31 @@ class RushApplicationsController < ApplicationController
 	def create
 		@application = Application.new(params[:application])
 		respond_to do |format|
-	    if @application.save
-	    	format.html { render nothing: true }
-	    	format.js do 
-	      	render :js => "					 
-												 $('#rush-application').delay(300).slideUp(1400);
-												 $('#apply').hide();
-												 $('.alert.alert-success').show(); "
-        end
-	    else
-	    	format.js do
-	    		render :js => "$('.alert.alert-danger').show();"
-	    	end
-	    end
-	  end
+			if @application.save
+				format.html { render nothing: true }
+				format.js do 
+					render :js => "
+					    $('#rush-application').delay(300).slideUp(1400);
+					    $('#apply').hide();
+					    $('.alert.alert-success').show(); 
+					"
+				end
+			else
+				format.js do
+					render :js => "$('.alert.alert-danger').show();"
+				end
+			end
+		end
 	end
 
 	def show
 		@application = Application.find(params[:id])
-			respond_to do |format|
-	      format.html
-	      format.pdf do
-	        render :pdf => "file_name",
-	        				:template => 'rush_applications/show.pdf.html.erb'
-	      end
-	    end
+		respond_to do |format|
+			format.html
+			format.pdf do
+				render :pdf => "file_name",
+				:template => 'rush_applications/show.pdf.html.erb'
+			end
+		end
 	end
 end
